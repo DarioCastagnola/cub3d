48a69e0bf19d7a42a9e5bdb5fc0d1e2d24e0d9db
Dario Castagnola <dcastagn@e3r2p5.42roma.it>
2023-06-30 16:47:23 +0200

    changes

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: dcastagn <dcastagn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:39:41 by dcastagn          #+#    #+#             */
-/*   Updated: 2023/06/28 12:13:13 by dcastagn         ###   ########.fr       */
+/*   Updated: 2023/06/30 15:37:05 by dcastagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,58 @@
 # include <math.h>
 # include <time.h>
 
-// ---------- TILES
+// risoluzione
+#define SCREEN_W 640
+#define SCREEN_H 480
+
+// Field Of View
+#define FOV 0.66
+
+// Pi
+#define PI 3.14159
+
+// passo di movimento e rotazione
+#define MOVSPEED 0.1
+#define ROTSPEED 0.05
+
+// ---------- STRUCTS
+
+// framebuffer
+typedef struct s_frame
+{
+    uint32_t width;
+    uint32_t height;
+    //TrueColorPixel *data;
+}	t_frame;
+
+// mappa
+typedef struct s_map
+{
+	char		**map;
+	t_vectors	player_pos;
+}	t_map;
+
+typedef struct s_vectors
+{
+	double	x;
+	double	y;
+}	t_vectors;
+
+// informazioni sul raggio
+typedef struct s_rayHit
+{
+    double distance;
+    int mapX;
+    int mapY;
+    double rayDirX;
+    double rayDirY;
+    int side;
+}	t_rayHit;
 
 typedef struct s_game
 {
 	void			*mlx;
 	void			*mlx_win;
-}		t_game;
+}	t_game;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dcastagn <dcastagn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:35:23 by dcastagn          #+#    #+#             */
-/*   Updated: 2023/06/28 12:20:54 by dcastagn         ###   ########.fr       */
+/*   Updated: 2023/06/30 16:39:29 by dcastagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,43 @@ int	ft_destroy_window(t_game *game)
 	mlx_destroy_window(game->mlx, game->mlx_win);
 	exit(printf("The evil forces reign on\n"));
 }
+
+void	initialization(t_game *game)
+{	
+	game->mlx = mlx_init();
+	game->mlx_win = mlx_new_window (game->mlx, SCREEN_W, SCREEN_H, "cub3d");
+}
+
 int	main(int argc, char **argv)
 {
 	t_game		game;
 
-    (void)argc;
-    (void)argv;
-	// if (argc != 2)
-	//     null_error("Wrong Input!!");
-	game.mlx = mlx_init();
-	//ft_check_input(argv[1]);
-	game.mlx_win = mlx_new_window(game.mlx, 860, 520, "cub3d");
+	if (argc != 2)
+	    null_error("Wrong Input!!");
+	ft_check_input(argv[1]);
+	initialization(&game);
 	mlx_hook(game.mlx_win, 17, 0, ft_destroy_window, &game);
 	mlx_loop(game.mlx);
+}
+
+int	find_player(t_map *map)
+{
+	int	y;
+	int	x;
+
+	y = -1;
+	while (map->map[++y])
+	{
+		x = -1;
+		while (map->map[y][++x])
+		{
+			if (ft_strchr("NSWE", map->map[y][x]))
+			{
+				map->player_pos.x = x;
+				map->player_pos.y = y;
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
